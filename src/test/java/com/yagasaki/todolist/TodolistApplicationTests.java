4c0ab34800f49a323ef647983d23002ca2db39e6
Anderson Marlon <anderson18.marlon@gmail.com>
2023-10-10 17:37:02 -0300

    Initial commit

---
+++ src/test/java/com/yagasaki/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yagasaki.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
