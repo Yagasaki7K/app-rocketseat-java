943d9611b63bb27aacea4302e2efa4fc3fc9477e
Anderson Marlon <anderson18.marlon@gmail.com>
2023-10-17 17:32:54 -0300

    Update Info by Windows

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # app-rocketseat-java
 A manager to create and delete your to do in list or To Do List
+
+# Relatório
+
+### Problemas que a comunidade da Kalify - que programam em Java - analisou e viu durante esse curso
+- Uso desnecessário de var dentro da classe e para manipular dados
+- Utilizar lombok e ignorar os primeiros fundamentos de Java
+- Ignorou Java em si e foi direto para Spring.
+- Não ensinou nada sobre super classe
+- Não ensinou nada sobre herança e generics
+- Não ensinou a usar o construtor padrão para fazer alteração
--- src/main/java/com/yagasaki/todolist/user/UserController.java
+++ src/main/java/com/yagasaki/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/user")
 
@@ -23,6 +25,10 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username already exists");
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
